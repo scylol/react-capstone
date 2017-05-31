df7ae169a7b431aabd6d12e8267078a0f9865c81
Scylol <djd3dobbin@gmail.com>
2017-05-31 17:33:52 -0400

    finished api endpoints

--- server/index.js
+++ server/index.js
@@ -1,12 +1,17 @@
 const path = require('path');
 const express = require('express');
 
+const bodyParser = require('body-parser');
+
+
 let knex = require('knex')({
   client: 'pg',
   connection: 'postgres://ryutqlmo:veCNdcwIHiGuFX9XjqdeebVGBkjV3UHb@stampy.db.elephantsql.com:5432/ryutqlmo'
 });
 
 const app = express();
+
+app.use(bodyParser.json());
 
 // API endpoints go here!
 
@@ -17,55 +22,63 @@ app.use(express.static(path.resolve(__dirname, '../client/build')));
 // Unhandled requests which aren't for the API should serve index.html so
 // client-side routing using browserHistory can function
 app.get(/^(?!\/api(\/|$))/, (req, res) => {
-    const index = path.resolve(__dirname, '../client/build', 'index.html');
-    res.sendFile(index);
+  const index = path.resolve(__dirname, '../client/build', 'index.html');
+  res.sendFile(index);
 });
 
-app.get('/api/users/:userId',(req,res,next)=>{
-    knex.insert({name:'name'}).into('users').then(e=>{
-      knex.select().from('users').then(function(e){
-        console.log(e);
-      });
-    })
-    res.send('Working!').end();
-})
+app.get('/api/users/:id',(req,res,next)=>{
+  let {id} = req.params;
+  knex.select().from('users').where('id', id).then(result => {
+    console.log(result);
+    res.json(result[0]).end();
+  });
+});
 
-app.put('/api/users',(req,res,next)=>{
-    res.send('Working!').end();
-})
+app.put('/api/users/:id',(req,res,next)=>{
+  knex('users')
+    .where('id', req.params.id)
+    .update({scores: req.body.scores})
+    .returning(['scores'])
+    .then(result => {
+      res.json(result[0]).end();
+    });
+
+});
 
 app.post('/api/users',(req,res,next)=>{
   //knex('users').insert({}).returning('*').toString();
-  knex('users').insert({name: "Anonymous"}).into('users').then(e=>{
-    console.log("done!");
-  })
+  knex('users').insert({name: req.body.name, scores: req.body.scores}).into('users').returning(['name', 'scores'])
+  .then(e=>{
+    console.log('done!');
+    res.send(e);
+  });
 
 });
 
 let server;
 function runServer(port=3001) {
-    return new Promise((resolve, reject) => {
-        server = app.listen(port, () => {
-            resolve();
-        }).on('error', reject);
-    });
+  return new Promise((resolve, reject) => {
+    server = app.listen(port, () => {
+      resolve();
+    }).on('error', reject);
+  });
 }
 
 function closeServer() {
-    return new Promise((resolve, reject) => {
-        server.close(err => {
-            if (err) {
-                return reject(err);
-            }
-            resolve();
-        });
+  return new Promise((resolve, reject) => {
+    server.close(err => {
+      if (err) {
+        return reject(err);
+      }
+      resolve();
     });
+  });
 }
 
 if (require.main === module) {
-    runServer();
+  runServer();
 }
 
 module.exports = {
-    app, runServer, closeServer
+  app, runServer, closeServer
 };
